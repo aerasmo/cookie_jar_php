19f9e00978a5f943d1f236912de89b446ed9124f
aerasmo <angelo1erasmo@gmail.com>
2023-01-16 11:29:13 +0800

    Vuln php page

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+  URL: <input type="text" name="url"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
 <?php
-	$cookie = $_GET["cookie"];
-?> 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $url = $_POST["url"];
+    if (empty($url)) {
+        $url = "/index.php";
+    }
+    echo "<a href='$url'>$url</a>";
+}
+?>
+
+</body>
+</html>
